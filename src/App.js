3fa0d19fc69fe0d907f1bb0f821b584dedde11f0
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2017-11-07 17:51:34 +0200

    added viewport styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './bundle.css';
 import CataloguePage from './pages/CataloguePage';
 import CollectionPage from './pages/CollectionPage';
 
+const viewportWidthMax = [1024, 1280, 1366, 1920, 2048, 3840];
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,38 +24,48 @@ class App extends Component {
     this.setState({ isCollectionUnmounted: mount });
   }
 
+  renderContent = () => {
+    switch (this.state.currentPageNum) {
+      case 0:
+        return (
+          <CataloguePage
+            isCollectionUnmounted={this.state.isCollectionUnmounted}
+            key="defaultCatalog"
+            setCatalogUnmounted={this.setCatalogUnmounted}
+            setCurrentPage={this.setCurrentPage}
+          />
+        );
+      case 1:
+        return (
+          <CollectionPage
+            isCatalogUnmounted={this.state.isCatalogUnmounted}
+            key="collection"
+            setCollectionUnmounted={this.setCollectionUnmounted}
+            setCurrentPage={this.setCurrentPage}
+          />
+        );
+      default:
+        return (
+          <CataloguePage
+            isCollectionUnmounted={this.state.isCollectionUnmounted}
+            key="defaultCatalog"
+            setCatalogUnmounted={this.setCatalogUnmounted}
+            setCurrentPage={this.setCurrentPage}
+          />
+        );
+    }
+  }
+
   render() {
-    return (() => {
-      switch (this.state.currentPageNum) {
-        case 0:
-          return (
-            <CataloguePage
-              isCollectionUnmounted={this.state.isCollectionUnmounted}
-              key="defaultCatalog"
-              setCatalogUnmounted={this.setCatalogUnmounted}
-              setCurrentPage={this.setCurrentPage}
-            />
-          );
-        case 1:
-          return (
-            <CollectionPage
-              isCatalogUnmounted={this.state.isCatalogUnmounted}
-              key="collection"
-              setCollectionUnmounted={this.setCollectionUnmounted}
-              setCurrentPage={this.setCurrentPage}
-            />
-          );
-        default:
-          return (
-            <CataloguePage
-              isCollectionUnmounted={this.state.isCollectionUnmounted}
-              key="defaultCatalog"
-              setCatalogUnmounted={this.setCatalogUnmounted}
-              setCurrentPage={this.setCurrentPage}
-            />
-          );
-      }
-    })();
+    // appears cobalt doesn't support media queries like browsers, so we have to apply classes based on window.innerWidth
+    const maxWidth = viewportWidthMax.find(width => window.innerWidth <= width);
+    const viewportWidthMaxClass = maxWidth ? ` viewport-width-${maxWidth}` : '';
+
+    return (
+      <div className={`content-holder${viewportWidthMaxClass}`}>
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
